9c2a6fc3f2553f5ff4ce5a45072d8e7607e3787e
Dawid Dutkiewicz <dawid.dutkiewicz@gmail.com>
2020-08-22 13:33:29 +0200

    Update test assertion

--- test_main.py
+++ test_main.py
@@ -23,7 +23,7 @@ class MainTest(unittest.TestCase):
 
     def test_hello_world(self):
         rv = self.app.get('/get_author/ulysses')
-        assert rv.data == 'James Joyce'
+        assert rv.data == b'James Joyce'
 
 if __name__ == '__main__':
     unittest.main()
